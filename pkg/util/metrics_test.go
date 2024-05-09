afc12e04e0863407f038762ae7d76791674c73e2
adolfo-ab <adolfoab6@gmail.com>
2024-05-09 20:12:56 +0200

    Add tests for getting the max and min number of tasks per file

--- pkg/util/metrics_test.go
+++ pkg/util/metrics_test.go
@@ -122,3 +122,40 @@ func TestGetAverageNumberOfTasksPerFile(t *testing.T) {
 	}
 
 }
+
+func TestGetNumberOfTasks(t *testing.T) {
+	t1 := entities.NewTask("content1", "file1", true)
+	t2 := entities.NewTask("content2", "file1", false)
+	t3 := entities.NewTask("content3", "file2", true)
+
+	tf1 := entities.TaskFile{Path: "file1", Tasks: []entities.Task{t1, t2}}
+	tf2 := entities.TaskFile{Path: "file2", Tasks: []entities.Task{t3}}
+	tf3 := entities.TaskFile{Path: "file3", Tasks: []entities.Task{t1}}
+	tf4 := entities.TaskFile{Path: "file4", Tasks: []entities.Task{}}
+	tf5 := entities.TaskFile{Path: "file5"}
+
+	var tests = []struct {
+		name     string
+		tfs      []entities.TaskFile
+		f        func([]entities.TaskFile) int
+		expected int
+	}{
+		{name: "MaxOneFileHasMoreTasks", tfs: []entities.TaskFile{tf1, tf2}, f: GetMaxNumberOfTasks, expected: 2},
+		{name: "MaxOneFileHasMoreTasks", tfs: []entities.TaskFile{tf1, tf2}, f: GetMinNumberOfTasks, expected: 1},
+		{name: "BothFilesHaveSameTasks", tfs: []entities.TaskFile{tf2, tf3}, f: GetMaxNumberOfTasks, expected: 1},
+		{name: "BothFilesHaveSameTasks", tfs: []entities.TaskFile{tf2, tf3}, f: GetMinNumberOfTasks, expected: 1},
+		{name: "EmptyTaskSlice", tfs: []entities.TaskFile{tf4}, f: GetMaxNumberOfTasks, expected: 0},
+		{name: "EmptyTaskSlice", tfs: []entities.TaskFile{tf4}, f: GetMinNumberOfTasks, expected: 0},
+		{name: "NoTasksAtAll", tfs: []entities.TaskFile{tf5}, f: GetMaxNumberOfTasks, expected: 0},
+		{name: "NoTasksAtAll", tfs: []entities.TaskFile{tf5}, f: GetMinNumberOfTasks, expected: 0},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := tt.f(tt.tfs)
+			if result != tt.expected {
+				t.Errorf("Incorrect number of tasks. Expected value: %d, actual value %d", tt.expected, result)
+			}
+		})
+	}
+}
